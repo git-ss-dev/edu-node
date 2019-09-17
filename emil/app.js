af14aeeb8cbe7816b2fb96169e2faf346b3d6846
Shivam Shrivastava <55176385+git-ss-dev@users.noreply.github.com>
2019-09-17 10:31:09 +0530

    Update app.js

--- emil/app.js
+++ emil/app.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-const sendgridAPIKey = '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'
+const sendgridAPIKey = 'API Key'
 sgMail.setApiKey(sendgridAPIKey)
 sgMail.send({
   to: 'shivam.shrivastava@edureka.co',
